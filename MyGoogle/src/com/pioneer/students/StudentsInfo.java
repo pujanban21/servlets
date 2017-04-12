afde900eb8a0a14fa8f1a0ba98e9e0e917c5182e
pujanban21 <pujan_567@hotmail.com>
2017-04-12 16:42:47 -0500

    servlets

---
+++ MyGoogle/src/com/pioneer/students/StudentsInfo.java
@@ -0,0 +1,23 @@
+package com.pioneer.students;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class StudentsInfo {
+
+	private HashMap<String, List<String>> studentsInfo;
+
+	public StudentsInfo() {
+		addList();
+	}
+
+	public HashMap<String, List<String>> addList() {
+
+		studentsInfo = new HashMap<>();
+		studentsInfo.put("Pujan", Arrays.asList("Pujan", "Ban", "25", "Nepal", "Texas Tech University"));
+		studentsInfo.put("Najar", Arrays.asList("Najar", "Aryal", "26", "Nepal", "Lamar University, Beaumont, TX"));
+		studentsInfo.put("Nirajan",Arrays.asList("Nirajan", "Thapa", "27", "Lamar University", "M.S. in Computer Science"));
+		return studentsInfo;
+	}
+}
